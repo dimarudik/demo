a34acc67084b1c4b32f4e20a98b96f108d01ab74
Dmitrii Rudik <dima.rudik@gmail.com>
2022-11-01 10:56:26 +0700

    + pgcat

--- src/main/resources/create_table.sql
+++ src/main/resources/create_table.sql
@@ -7,3 +7,8 @@ CREATE TABLE public.payments_tab (
 );
 ALTER TABLE public.payments_tab OWNER TO shard;
 ALTER TABLE ONLY public.payments_tab ADD CONSTRAINT payments_tab_pkey PRIMARY KEY (uuid);
+
+curl -i -X POST http://localhost:8080/payment -H "Content-Type:application/json" -d '{"clientId": "5-339CVCCVD", "amount": "3000", "timestamp": "2022-08-04T15:00:48", "status": "I"}'
+curl -i -X POST http://localhost:8080/payment -H "Content-Type:application/json" -d '{"clientId": "5-6PAMOQ6V", "amount": "250", "timestamp": "2022-08-04T15:00:48", "status": "I"}'
+curl -i -X POST http://localhost:8080/payment -H "Content-Type:application/json" -d '{"clientId": "5-ABELKTQM", "amount": "3000", "timestamp": "2022-08-04T15:00:48", "status": "I"}'
+curl -i -X POST http://localhost:8080/payment -H "Content-Type:application/json" -d '{"clientId": "5-CLS23Y90", "amount": "4.09", "timestamp": "2022-08-04T15:00:48", "status": "I"}'
