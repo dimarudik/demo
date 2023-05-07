e181a628ae56a4c63e4a9c739671330c2d8e3ab9
Dmitrii Rudik <dima.rudik@gmail.com>
2023-05-07 09:05:06 +0300

    odyssey moved from demo

--- k8s/postrgres-database/init.sql
+++ k8s/postrgres-database/init.sql
@@ -3,3 +3,5 @@ create role test login password 'test';
 create role odyssey login superuser password 'odyssey';
 create role pgbouncer login password 'pgbouncer';
 grant select on pg_shadow to pgbouncer;
+create table test (id int primary key, name text);
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO test;
